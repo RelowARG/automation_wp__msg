e65d82949aca12048f38125fd42c65a2de9e806a
Relow <juliangrlw@gmail.com>
2022-07-20 21:02:21 -0300

    ,

--- test/exportWhatsappContact.py
+++ test/exportWhatsappContact.py
@@ -1,2 +1,17 @@
-from datetime import datetime, date, timedelta
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import time 
 
+# driver = webdriver.Chrome(ChromeDriverManager().install())
+
+# AjtLy _1FXE6 _1lF7t
+
+driver = webdriver.Chrome("D:\\Visual Studio Code\\automation_wp_msg\\test\\chromedriver.exe")
+driver.get("https://web.whatsapp.com/")
+
+time.sleep(15)
+
+content = driver.find_element(By.CLASS_NAME, "AjtLy _1FXE6 _1lF7t")
+
+print(content)
